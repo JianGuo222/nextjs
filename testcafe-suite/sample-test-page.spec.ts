a44e9d531e73c76ea3973b1fb4a437a860af4567
Jian Guo <jian.guo@coles.com.au>
2021-06-15 11:07:22 +1000

    add testcafe

---
+++ testcafe-suite/sample-test-page.spec.ts
@@ -0,0 +1,11 @@
+import { Selector } from "testcafe";
+
+fixture`Getting Started` // declare the fixture
+  .page`https://nextjs-steel-kappa-16.vercel.app/`; // specify the start page
+
+//then create a test and place your code there
+test('homepage', async (t) => {
+  await t
+  .expect(Selector('h1').innerText)
+  .eql('Welcome to Next.js!');
+});
